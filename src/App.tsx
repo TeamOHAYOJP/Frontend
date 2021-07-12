9c550afbe4524b1ca0bdee02dadbf46a990d9749
moyongkexing <moyongkexing@gmail.com>
2021-07-13 00:34:09 +0900

    動作確認用APIクライアントを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import { VFC } from 'react';
+import { useState, useEffect, VFC } from 'react';
+import { execTest } from "lib/api/test"
+
 import './App.css';
 
 const App: VFC = () => {
-  
+  const [message, setMessage] = useState<string>("")
+
+  const handleExecTest = async () => {
+    const res = await execTest()
+
+    if (res.status === 200) {
+      setMessage(res.data.message)
+    }
+  }
+
+  useEffect(() => {
+    handleExecTest()
+  }, [])
+
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <h1>{message}</h1>
+  )
 }
 
 export default App;
