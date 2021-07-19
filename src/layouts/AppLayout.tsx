6a8e7212933c745dd5f9ce221df49de7d702341f
Leandro <leandro83g@icloud.com>
2021-07-19 22:42:36 +0900

    必要ない部分を消去

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -3,7 +3,7 @@ import React from "react"
 import { Container, Grid } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 
-import { Header, HeaderDefault } from "layouts/Header"
+import { HeaderDefault } from "layouts/Header"
 
 const useStyles = makeStyles(() => ({
     container: {
