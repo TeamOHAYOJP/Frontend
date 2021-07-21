67f85a6af820012fc86a07b86801254b7dbf1b3e
Leandro <leandro83g@icloud.com>
2021-07-21 11:29:44 +0900

    RankingRecordにリンクをつけた

--- src/pages/Ranking.tsx
+++ src/pages/Ranking.tsx
@@ -4,15 +4,13 @@ import { useState, useEffect } from "react";
 import { getRankings } from "lib/api/ranking";
 import { DailyRanking } from "interfaces/ranking";
 
-import { RankingRecord } from "components/RankingRecord";
+import { RankingRecord , RankingTopRecord } from "components/RankingRecord";
 
-import PersonIcon from "@material-ui/icons/Person"
-import EmojiEventsIcon from '@material-ui/icons/EmojiEvents';
 
 export const Ranking: VFC = ()=>{
 
     const [ rankings, setRankings ] = useState<DailyRanking[]>([])
-
+    
     const handleRankings = async () => {
 
         try{
@@ -30,34 +28,26 @@ export const Ranking: VFC = ()=>{
     useEffect(()=>{
         handleRankings()
     },[])
-
-    // TODO: 一位だけ切り取って2番目からレコード表示にしないと
-    // TODO: スタイリングを少し
     
+    
+    // TODO: 無理矢理だがひとまずこれで、他の方法があればそれに変更したい。
+    // TODO: component:childernをうまく使えばいける？
     return (
         <>
-
-        <div className="bg-red rounded-full h-64 w-64 flex justify-center items-start m-auto p-4 " >
             <div>
-                <div className="flex justify-center items-center">
-                    <EmojiEventsIcon  fontSize="large" htmlColor="#F7FD04" />
-                </div>
-                <div className="bg-white rounded-full h-20 w-20 flex justify-center items-center" >
-                    <PersonIcon fontSize="large"/>
-                </div>
-                <p className="text-center">{"Leandro"}</p>
-                <p className="text-center text-green ">{"7:30"}</p>
+                {
+                    rankings.map((ranking, idx)=> idx !== 0 ? (<></>):(
+                        <RankingTopRecord {...ranking}/>
+                    ))
+                }
             </div>
-
-        </div>
-
-        <div className="">
-            {
-                rankings.map(r => (
-                    <RankingRecord {...r}/>
-                ))
-            }
-        </div>
+            <div className="-mt-10">
+                {
+                    rankings.map((ranking, idx)=> idx === 0 ? (<></>):(
+                        <RankingRecord {...ranking}/>
+                    ))
+                }
+            </div>
         </>
     )
 }
